bcd812dbf6ed5f59e16a2fc38c315a3011bd219e
Saurabh Khandelwal <stgstg2713@gmail.com>
2019-03-19 17:56:09 +0530

    Prediction code

---
+++ predict.py
@@ -0,0 +1,28 @@
+###Prediction for election based on twitter data########
+####### Author : Saurabh Khandelwal ####################
+
+import keras
+
+def load_sentiment_model(filename):
+	pass
+
+
+def load_data():
+	pass
+
+
+def visualize():
+	pass
+
+
+def predict():
+	pass
+
+if __name__ == '__main__':
+	print ("################################## Prediction Start #################################")
+	print (" load_sentiment_model................................................................")
+	model = load_sentiment_model()
+	data = load_data()
+	predict(data)
+
+
